435fce48d1630a0125dff995ee969763cae5a762
Maiilen <103055484+Maiilen@users.noreply.github.com>
2022-08-02 10:28:40 -0300

    Componentes agregados

--- proyectoIntegrador_YMO/src/app/app.module.ts
+++ proyectoIntegrador_YMO/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercademiComponent } from './componentes/acercademi/acercademi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    AcercademiComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
